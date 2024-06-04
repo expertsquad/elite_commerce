58059d032a3f3c608969bf24d046e68935aef91b
nurulislamrimon <nurulislamrimon@gmail.com>
2024-06-04 12:52:23 +0600

    color setup done

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,26 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    // use color variables please
+    // colors
+    colors: {
+      positive: "var(--positive)",
+      transparent: "var(--transparent)",
+      white: { 15: "var(--white-15)", DEFAULT: "var(--white)" },
+      black: {
+        50: "var(--black-50)",
+        80: "var(--black-80)",
+        DEFAULT: "var(--black)",
+      },
+    },
+    // gradient colors
+    backgroundImage: {
+      "gradient-primary": "var(--gradient-primary)",
+      "gradient-primary-light": "var(--gradient-primary-light)",
+      "gradient-secondary": "var(--gradient-secondary)",
+      "gradient-secondary-light": "var(--gradient-secondary-light)",
+      "gradient-positive": "var(--gradient-positive)",
+    },
   },
   plugins: [],
 };
