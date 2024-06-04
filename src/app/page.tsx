69bebb1e96316896b5147982e91c225c53e5a6de
nurulislamrimon <nurulislamrimon@gmail.com>
2024-06-04 13:26:49 +0600

    garbage removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
 import { Button } from "./Components/Buttons";
 
+export const metadata = {
+  title: "Home",
+  description: "Home page",
+  keywords: "Home, page",
+  image: "https://www.example.com/image.png",
+  url: "https://www.example.com",
+};
+
 export default function Home() {
   return (
     <main>
