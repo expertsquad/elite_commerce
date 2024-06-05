cf054fcc04f2d512c8c4899b01d57f349a97012c
nurulislamrimon <nurulislamrimon@gmail.com>
2024-06-05 21:59:46 +0600

    props in gradient generator done

--- src/app/(main-layout)/page.tsx
+++ src/app/(main-layout)/page.tsx
@@ -1,13 +1,7 @@
 import React from "react";
-import GradientIconTest from "../___ExampleComponent/GradientIconTest";
 
 const page = () => {
-  return (
-    <div>
-      <GradientIconTest />
-      Main
-    </div>
-  );
+  return <div>Main</div>;
 };
 
 export default page;
