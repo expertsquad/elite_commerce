8250e5afcd9812b5ed3e2d910685954d3bb270b6
nurulislamrimon <nurulislamrimon@gmail.com>
2024-06-05 22:07:19 +0600

    gradient icon generator added

--- src/app/___ExampleComponent/GradientIconTest.tsx
+++ src/app/___ExampleComponent/GradientIconTest.tsx
@@ -6,6 +6,7 @@ const GradientIconTest = () => {
   return (
     <div>
       <GenerateGradientIcon IconComponent={IconX} stroke={5} />
+      <GenerateGradientIcon IconComponent={IconTrash} stroke={5} />
     </div>
   );
 };
