8b756c73d0d600b467b630d534b5c2014e985e8e
nurulislamrimon <nurulislamrimon@gmail.com>
2024-06-05 19:48:29 +0600

    layout added

---
+++ src/app/(main-layout)/_components/Header.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+  return <div className="bg-gradient-primary-light h-16">Header</div>;
+};
+
+export default Header;
---
+++ src/app/(main-layout)/layout.tsx
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react";
+import Header from "./_components/Header";
+import Footer from "./_components/Footer";
+
+const MainLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main>
+      <Header />
+      <section className="max-w-7xl mx-auto p-3">{children}</section>
+      <Footer />
+    </main>
+  );
+};
+
+export default MainLayout;
