25eb5f49eb77d0ccab694fde30c9e1fc731bdc92
nurulislamrimon <nurulislamrimon@gmail.com>
2024-06-05 23:31:12 +0600

    black color updated

--- src/app/(main-layout)/_components/Header.tsx
+++ src/app/(main-layout)/_components/Header.tsx
@@ -1,7 +1,48 @@
+import Link from "next/link";
 import React from "react";
 
 const Header = () => {
-  return <div className="bg-gradient-primary-light h-16">Header</div>;
+  return (
+    <header className="py-[clamp(8px,2vh,20px)] px-[clamp(10px,5vw,30px)] text-md bg-gradient-primary-light flex items-center justify-between">
+      <div className="flex gap-5 items-center">
+        <span className="text-2xl">Logo</span>
+        <ul className="flex gap-3">
+          <li>
+            <Link href="#">data</Link>
+          </li>
+          <li>
+            <Link href="#">data</Link>
+          </li>
+          <li>
+            <Link href="#">data</Link>
+          </li>
+          <li>
+            <Link href="#">data</Link>
+          </li>
+          <li>
+            <Link href="#">data</Link>
+          </li>
+          <li>
+            <Link href="#">data</Link>
+          </li>
+          <li>
+            <Link href="#">data</Link>
+          </li>
+          <li>
+            <Link href="#">data</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="flex gap-3">
+        <span className="first:after:content-['_|'] after:text-black-50 after:opacity-50">
+          Icon
+        </span>
+        <span>Icon</span>
+        <span>Icon</span>
+        <span>Icon</span>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
