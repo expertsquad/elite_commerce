0eeaaf2ed19e2477b85edb893a4392eafd2d8f51
nurulislamrimon <nurulislamrimon@gmail.com>
2024-06-06 23:43:32 +0600

    category component in menu separated

--- src/app/(main-layout)/_components/HeaderComponents/LargeDeviceMegaMenu.tsx
+++ src/app/(main-layout)/_components/HeaderComponents/LargeDeviceMegaMenu.tsx
@@ -14,9 +14,39 @@ import FeaturedCard from "./FeaturedCard";
 import { fetchData } from "@/actions/fetchData";
 import { ICategory } from "@/interfaces/category.interface";
 
-const LargeDeviceMegaMenu = async () => {
+const CategoriesAndSubcategories = async () => {
   const categories = await fetchData({ route: "/category" });
 
+  return categories?.data?.map((category: ICategory) => (
+    <li key={category?._id} className="group/category">
+      <Link
+        href="#"
+        className="flex w-full p-2 group-hover/category:bg-gradient-primary-light justify-between"
+      >
+        {category?.categoryName}
+        <IconChevronRight className="text-black-50 hidden group-hover/category:block" />
+      </Link>
+      {/* ================= sub categories ================== */}
+      <div className="fixed top-0 left-[196px] bg-white opacity-0 h-0 invisible transition-all duration-300 group-hover/category:visible group-hover/category:opacity-100 group-hover/category:h-[clamp(100px,70vh,600px)] backdrop-blur-xl py-2 shadow-xl flex">
+        <ul className="w-48 h-full flex flex-col overflow-auto">
+          {category?.subcategories?.map((subcategory) => (
+            <li key={subcategory?.subcategoryId}>
+              <Link
+                href="#"
+                className="block w-full p-2 hover:bg-gradient-primary-light"
+              >
+                {subcategory?.subcategoryName}
+              </Link>
+            </li>
+          ))}
+        </ul>
+        <FeaturedCard />
+      </div>
+    </li>
+  ));
+};
+
+const LargeDeviceMegaMenu = () => {
   const iconStroke = 1.5;
   return (
     <nav className="hidden md:flex max-w-7xl mx-auto py-[clamp(8px,2vh,20px)] px-[clamp(10px,5vw,30px)] text-md items-center justify-between">
@@ -33,43 +63,12 @@ const LargeDeviceMegaMenu = async () => {
               <IconChevronDown className="text-black-50 group-hover/categorybtn:hidden" />
               <IconChevronUp className="text-black-50 hidden group-hover/categorybtn:block" />
             </Link>
-            {/* ========================================================== */}
-            {/* ================= categories start ================== */}
-            {/* ========================================================== */}
-            <ul className="absolute bg-white opacity-0 h-0 invisible transition-all duration-300 group-hover/categorybtn:visible group-hover/categorybtn:opacity-100 group-hover/categorybtn:h-[clamp(100px,70vh,600px)] backdrop-blur-xl w-48 shadow-xl py-2 flex flex-col">
-              {" "}
-              {categories?.data?.map((category: ICategory) => (
-                <li key={category?._id} className="group/category">
-                  <Link
-                    href="#"
-                    className="flex w-full p-2 group-hover/category:bg-gradient-primary-light justify-between"
-                  >
-                    {category?.categoryName}
-                    <IconChevronRight className="text-black-50 hidden group-hover/category:block" />
-                  </Link>
-                  {/* ========================================================== */}
-                  {/* ================= sub categories start ================== */}
-                  {/* ========================================================== */}
-                  <div className="absolute bg-white opacity-0 h-0 invisible transition-all duration-300 group-hover/category:visible group-hover/category:opacity-100 group-hover/category:h-[clamp(100px,70vh,600px)] backdrop-blur-xl py-2 top-0 left-[196px] shadow-xl flex">
-                    <ul className="w-48 h-full flex flex-col overflow-auto">
-                      {category?.subcategories?.map((subcategory) => (
-                        <li key={subcategory?.subcategoryId}>
-                          <Link
-                            href="#"
-                            className="block w-full p-2 hover:bg-gradient-primary-light"
-                          >
-                            {subcategory?.subcategoryName}
-                          </Link>
-                        </li>
-                      ))}
-                    </ul>
-                    <FeaturedCard />
-                  </div>
-                  {/* ================= sub categories end ================== */}
-                </li>
-              ))}
+            <ul className="absolute bg-white opacity-0 h-0 invisible transition-all duration-300 group-hover/categorybtn:visible group-hover/categorybtn:opacity-100 group-hover/categorybtn:h-[clamp(100px,70vh,600px)] backdrop-blur-xl w-48 shadow-xl py-2 flex flex-col ">
+              {/* ========================================================== */}
+              {/* ================= categories ================== */}
+              {/* ========================================================== */}
+              <CategoriesAndSubcategories />
             </ul>
-            {/* ================= categories end ================== */}
           </li>
 
           {/* ======================== menus ========================== */}
