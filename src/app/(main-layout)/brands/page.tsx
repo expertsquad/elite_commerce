0f7944d8a193ace6a97aaf03a13c7c7d06dac2d5
Shariful525 <sharifulafsar0@gmail.com>
2024-06-08 17:14:46 +0600

    brands page are now responsive

--- src/app/(main-layout)/brands/page.tsx
+++ src/app/(main-layout)/brands/page.tsx
@@ -5,37 +5,25 @@ import FilterSort from "./_components/FilterSort";
 import Pagination from "./_components/Pagination";
 import { IBrand } from "@/interfaces/brand.interface";
 
-// export async function generateStaticParams() {
-//   const brands = await fetchData({
-//     route: "/brand",
-//   });
-//   const totalPages = Math.ceil(brands?.meta?.total / brands?.meta?.limit);
-//   return [...Array(totalPages)].map((page) => ({
-//     page: page.toString(),
-//   }));
-// }
-
-// export async function getStaticProps() {
-//   const brands = await fetchData({
-//     route: "/brand",
-//   });
-//   const totalPages = Math.ceil(brands?.meta?.total / brands?.meta?.limit);
-//   return [...Array(totalPages)].map((page) => ({
-//     page: page.toString(),
-//   }));
-// }
-
-const Brand = async (props) => {
-  console.log(props);
+const Brand = async ({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) => {
+  const currentPage = searchParams?.page ? parseInt(searchParams?.page) : 1;
+  // console.log(currentPage);
   const brandData = await fetchData({
     route: "/brand",
+    page: currentPage,
   });
 
   const totalPages = Math.ceil(brandData?.meta?.total / brandData?.meta?.limit);
-  // console.log();
 
   return (
-    <div className="flex flex-col gap-5 md:gap-7 md:col-span-3">
+    <div className="flex flex-col gap-5 md:gap-7 md:col-span-3 ">
       <div className="flex items-center justify-between">
         <span className="md:font-semibold md:text-xl text-base">
           {brandData?.meta?.total} Brands Found Here
@@ -44,7 +32,7 @@ const Brand = async (props) => {
           <FilterSort />
         </div>
       </div>
-      <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1  gap-5 overflow-y-auto">
+      <div className="grid grid-cols-brand-card-grid gap-5">
         {brandData?.data?.map(
           (brand: {
             _id: string;
@@ -57,20 +45,10 @@ const Brand = async (props) => {
         )}
       </div>
       <div>
-        {/* <Pagination totalPages={totalPages} currentPage={params.page} /> */}
+        <Pagination totalPages={totalPages} currentPage={currentPage} />
       </div>
     </div>
   );
 };
 
 export default Brand;
-
-export async function getStaticProps() {
-  const brands = await fetchData({
-    route: "/brand",
-  });
-  const totalPages = Math.ceil(brands?.meta?.total / brands?.meta?.limit);
-  return [...Array(totalPages)].map((page) => ({
-    page: page.toString(),
-  }));
-}
