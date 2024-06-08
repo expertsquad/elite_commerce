7c5d4b74120ae6d5e1ad773de366454d0cd72f41
Zayed Hossain <zayedhossain120@gmail.com>
2024-06-08 19:36:58 +0600

    dfkj

--- src/app/(main-layout)/profile/_components/OrderItems.tsx
+++ src/app/(main-layout)/profile/_components/OrderItems.tsx
@@ -18,9 +18,21 @@ const OrderItems = async () => {
       {/* Order top section start */}
       <div className="flex items-center md:justify-between flex-col lg:flex-row  border border-transparent lg:border lg:border-black-10 px-0 lg:px-4 py-3 rounded-lg gap-5">
         <div className="flex  gap-3 justify-between lg:justify-around border border-black-10 rounded-lg lg:border-transparent w-full lg:w-7/12 p-3 ">
-          <OrderCardHeader title="Order ID" value="1234567890" />
-          <OrderCardHeader title="Order Date" value="12/12/2021" />
-          <OrderCardHeader title="Estimated Delivery" value="12/12/2021" />
+          <OrderCardHeader
+            title="Order ID"
+            value="1234567890"
+            className="font-bold"
+          />
+          <OrderCardHeader
+            title="Order Date"
+            value="12/12/2021"
+            className="font-bold"
+          />
+          <OrderCardHeader
+            title="Estimated Delivery"
+            value="12/12/2021"
+            className="font-bold"
+          />
         </div>
         <div className="flex  items-center gap-3">
           <Link href="#" className="">
--- src/app/(main-layout)/profile/_components/OrderItemsCard.tsx
+++ src/app/(main-layout)/profile/_components/OrderItemsCard.tsx
@@ -1,7 +1,16 @@
+import Image from "next/image";
 import React from "react";
 
 const OrderItemsCard = () => {
-  return <div>Hello order items</div>;
+  return (
+    <div>
+      <div>
+        <Image alt="Product Image" height={100} src="/" width={100} />
+      </div>
+      <p>Here will out titile</p>
+      <small>Brand</small>
+    </div>
+  );
 };
 
 export default OrderItemsCard;
