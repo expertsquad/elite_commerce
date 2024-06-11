eebdfc63e95cc360072a5879fe6b7deee86e479a
nurulislamrimon <nurulislamrimon@gmail.com>
2024-06-11 21:45:37 +0600

    favourite brand section in home processing

--- src/app/(main-layout)/page.tsx
+++ src/app/(main-layout)/page.tsx
@@ -7,6 +7,7 @@ import FeaturedProducts from "./_components/FeaturedProducts/FeaturedProducts";
 import { fetchData } from "@/actions/fetchData";
 import BestDealsSection from "./_components/BestDealsSection/BestDealsSection";
 import { extraServices } from "@/constants/extraServices.constants";
+import WidgetCard from "@/Components/WidgetCard";
 
 const page = async () => {
   const newestProducts = await fetchData({ route: "/product" });
@@ -53,25 +54,41 @@ const page = async () => {
       {/* best deals */}
       <BestDealsSection />
       {/* extra services section */}
-      <div className="max-w-7xl mx-auto p-3 flex flex-col md:flex-row items-center justify-between py-10 lg:py-16 text-center md:text-left gap-10">
-        {extraServices.map((service) => (
-          <div
-            className="flex flex-col md:flex-row md:gap-2 items-center"
-            key={service.title}
-          >
-            <Image
-              src={service.icon}
-              alt="delivery truck icon"
-              className="w-12 h-12 md:w-8 md:h-8 lg:w-12 lg:h-12"
-            />
-            <div>
-              <h5 className="font-bold text-sm lg:text-sm">{service.title}</h5>
-              <p className="text-black-50 text-xs lg:text-sm">
-                {service.tagline}
-              </p>
+      <div className="max-w-7xl mx-auto p-3">
+        <div className="flex flex-col md:flex-row items-center justify-between py-10 lg:py-16 text-center md:text-left gap-10">
+          {extraServices.map((service) => (
+            <div
+              className="flex flex-col md:flex-row md:gap-2 items-center"
+              key={service.title}
+            >
+              <Image
+                src={service.icon}
+                alt="delivery truck icon"
+                className="w-12 h-12 md:w-8 md:h-8 lg:w-12 lg:h-12"
+              />
+              <div>
+                <h5 className="font-bold text-sm lg:text-sm">
+                  {service.title}
+                </h5>
+                <p className="text-black-50 text-xs lg:text-sm">
+                  {service.tagline}
+                </p>
+              </div>
             </div>
+          ))}
+        </div>
+        {/* favourite brand section */}
+        <div className="flex items-start gap-5">
+          {/* wiget card */}
+          <div className="w-[400px]">
+            <WidgetCard />
           </div>
-        ))}
+          <div className="w-full relative after:content-[''] after:absolute inset-0 ">
+            <h1 className="text-2xl font-bold text-gradient-primary">
+              Explore your Favorite Brand
+            </h1>
+          </div>
+        </div>
       </div>{" "}
     </>
   );
