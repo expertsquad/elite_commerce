16d580b30318dc1388b8447c2704801c1e0d6a58
Shariful525 <sharifulafsar0@gmail.com>
2024-06-11 11:08:44 +0600

    quick order modal created

--- src/app/(main-layout)/brands/_components/QuickOrderModal.tsx
+++ src/app/(main-layout)/brands/_components/QuickOrderModal.tsx
@@ -67,7 +67,7 @@ export const QuickOrderItem = () => {
 
 export const OrderSummery = () => {
   return (
-    <div className="md:border border-black-10 rounded-[10px] px-5 py-3.5  space-y-2.5">
+    <div className="md:border border-black-10 rounded-[10px] px-5 py-3.5  space-y-2.5 bg-white">
       <div className="flex items-center justify-between ">
         <span className="text-black-80 md:text-base text-sm">Subtotal</span>
         <strong className="text-black-80 md:text-base text-sm font-semibold">
@@ -117,105 +117,128 @@ const QuickOrderModal = ({
       show={show}
       setShow={setShow}
       alignment="center"
-      className="md:w-2/4 w-full h-[800px] overflow-y-auto"
+      className="  h-[800px] overflow-y-auto"
     >
-      <div className="flex justify-between gap-5 md:flex-row flex-col-reverse md:p-0 p-3.5 relative">
-        <button
-          onClick={() => setShow(false)}
-          className="absolute right-5 top-5"
-        >
-          <IconX />
-        </button>
-        <div className="flex-1 flex  border-r border-black-10  flex-col pr-5 ">
-          <div className="md:flex flex-col gap-3.5 hidden">
-            <span className="font-semibold  text-lg md:block hidden">
-              Quick Order
-            </span>
-            <input
-              type="range"
-              className="w-full  pointer-events-none"
-              value={10}
-            />
-            <span className="block text-base">
-              Buy <span className="text-gradient-primary">$900</span> more to
-              get{" "}
-              <span className="text-gradient-primary font-semibold">
-                Freeship
-              </span>{" "}
-              🔥
-            </span>
+      <div className="">
+        <div className="flex justify-between gap-5 md:flex-row flex-col-reverse md:p-0 p-3.5 relative">
+          <button
+            onClick={() => setShow(false)}
+            className="absolute right-5 top-5"
+          >
+            <IconX />
+          </button>
+          <div className="flex-1 flex  border-r border-black-10  flex-col pr-5 ">
+            <div className="md:flex flex-col gap-3.5 hidden">
+              <span className="font-semibold  text-lg md:block hidden">
+                Quick Order
+              </span>
+              <input
+                type="range"
+                className="w-full  pointer-events-none"
+                value={10}
+              />
+              <span className="block text-base">
+                Buy <span className="text-gradient-primary">$900</span> more to
+                get{" "}
+                <span className="text-gradient-primary font-semibold">
+                  Freeship
+                </span>{" "}
+                🔥
+              </span>
+            </div>
+            <hr className="border-black-10 my-5" />
+            <div className="flex flex-col gap-5 overflow-y-auto h-[600px]">
+              {[...Array(10)].map((item, index) => (
+                <QuickOrderItem key={index} />
+              ))}
+            </div>
           </div>
-          <hr className="border-black-10 my-5" />
-          <div className="flex flex-col gap-5 overflow-y-auto h-[600px]">
-            {[...Array(10)].map((item, index) => (
-              <QuickOrderItem key={index} />
-            ))}
+          <div className="flex-1">
+            <div className="md:hidden flex-col gap-3.5 flex my-10">
+              <span className="font-semibold  text-lg md:block hidden">
+                Quick Order
+              </span>
+              <input
+                type="range"
+                className="w-full  pointer-events-none"
+                value={10}
+              />
+              <span className="block text-base">
+                Buy <span className="text-gradient-primary">$900</span> more to
+                get{" "}
+                <span className="text-gradient-primary font-semibold">
+                  Freeship
+                </span>{" "}
+                🔥
+              </span>
+            </div>
+            <div className="flex flex-col gap-2.5">
+              <span className="uppercase font-semibold text-lg">
+                Cash on delivery
+              </span>
+              <span className="text-black-50 text-base">
+                Enter Your shipping address
+              </span>
+            </div>
+
+            <form action="" className="mt-12 flex flex-col gap-[50px]">
+              <div className="flex flex-col gap-5  ">
+                <div className="flex flex-col gap-2.5">
+                  <label htmlFor="fullName">
+                    Full Name <span className="text-gradient-secondary">*</span>
+                  </label>
+                  <div className="border border-black-10 rounded-lg px-5 py-3 flex justify-between items-center">
+                    <input
+                      type="text"
+                      name="fullName"
+                      className="outline-none w-full"
+                    />
+                    <IconUser stroke={1} />
+                  </div>
+                </div>
+                <div className="flex flex-col gap-2.5">
+                  <label htmlFor="userPhone">
+                    Phone Number{" "}
+                    <span className="text-gradient-secondary">*</span>
+                  </label>
+                  <div className="border border-black-10 rounded-lg px-5 py-3 flex justify-between items-center">
+                    <input
+                      type="text"
+                      name="userPhone"
+                      className="outline-none w-full"
+                    />
+                    <IconPhone stroke={1} />
+                  </div>
+                </div>
+                <div className="flex flex-col gap-2.5">
+                  <label htmlFor="userAddress">
+                    Address <span className="text-gradient-secondary">*</span>
+                  </label>
+                  <div className="border border-black-10 rounded-lg px-5 py-3 flex justify-between items-center">
+                    <input
+                      type="text"
+                      name="userAddress"
+                      className="outline-none w-full"
+                    />
+                    <IconMapPin stroke={1} />
+                  </div>
+                </div>
+              </div>
+              <div className="md:block hidden">
+                <OrderSummery />
+              </div>
+            </form>
+            <Link
+              href={"/"}
+              className="uppercase text-black-80 md:flex items-center justify-center mt-5 gap-2  hidden"
+            >
+              <IconArrowLeft />
+              <span>Continue Shopping</span>
+            </Link>
           </div>
         </div>
-        <div className="flex-1">
-          <div className="flex flex-col gap-2.5">
-            <span className="uppercase font-semibold text-lg">
-              Cash on delivery
-            </span>
-            <span className="text-black-50 text-base">
-              Enter Your shipping address
-            </span>
-          </div>
-
-          <form action="" className="mt-12 flex flex-col gap-[50px]">
-            <div className="flex flex-col gap-5  ">
-              <div className="flex flex-col gap-2.5">
-                <label htmlFor="fullName">
-                  Full Name <span className="text-gradient-secondary">*</span>
-                </label>
-                <div className="border border-black-10 rounded-lg px-5 py-3 flex justify-between items-center">
-                  <input
-                    type="text"
-                    name="fullName"
-                    className="outline-none w-full"
-                  />
-                  <IconUser stroke={1} />
-                </div>
-              </div>
-              <div className="flex flex-col gap-2.5">
-                <label htmlFor="userPhone">
-                  Phone Number{" "}
-                  <span className="text-gradient-secondary">*</span>
-                </label>
-                <div className="border border-black-10 rounded-lg px-5 py-3 flex justify-between items-center">
-                  <input
-                    type="text"
-                    name="userPhone"
-                    className="outline-none w-full"
-                  />
-                  <IconPhone stroke={1} />
-                </div>
-              </div>
-              <div className="flex flex-col gap-2.5">
-                <label htmlFor="userAddress">
-                  Address <span className="text-gradient-secondary">*</span>
-                </label>
-                <div className="border border-black-10 rounded-lg px-5 py-3 flex justify-between items-center">
-                  <input
-                    type="text"
-                    name="userAddress"
-                    className="outline-none w-full"
-                  />
-                  <IconMapPin stroke={1} />
-                </div>
-              </div>
-            </div>
-            <div className="">
-              <OrderSummery />
-            </div>
-          </form>
-          <Link
-            href={"/"}
-            className="uppercase text-black-80 flex items-center justify-center mt-5 gap-2"
-          >
-            <IconArrowLeft />
-            <span>Continue Shopping</span>
-          </Link>
+        <div className="md:hidden mx-auto w-[90%]">
+          <OrderSummery />
         </div>
       </div>
     </Modal>
