a58022bde9509962c30ca8b1899e38c43b0884b9
Zayed Hossain <zayedhossain120@gmail.com>
2024-06-13 11:21:06 +0600

    pulling main

--- src/app/(main-layout)/profile/account-details/_components/PersonalInformation.tsx
+++ src/app/(main-layout)/profile/account-details/_components/PersonalInformation.tsx
@@ -1,44 +1,55 @@
 import { fetchProtectedData } from "@/actions/fetchData";
 import { Button } from "@/Components/Buttons";
 import CustomInput from "@/Components/CustomInput";
+import Form from "@/Components/Form";
 
 const PersonalInformation = async () => {
   const getMe = await fetchProtectedData({
     route: "/user/me",
   });
 
+  const handleSubmit = async (formData: FormData) => {
+    "use server";
+  };
+
   return (
-    <div>
+    <div className="">
       <h3 className="[font-size:_clamp(1em,5vw,1.5em)] font-semibold text-gradient-primary my-7 ">
         Personal Information
       </h3>
+      <Form handleSubmit={handleSubmit}>
+        <div className="grid grid-cols-1 gap-4 md:gap-6 lg:grid-cols-2 w-full">
+          <CustomInput
+            label="Full Name"
+            type="text"
+            name="fullName"
+            placeholder="Zayed Hossain"
+            defaultValue={getMe?.data?.fullName}
+          />
+          <CustomInput
+            label="Email"
+            type="email"
+            name="email"
+            defaultValue={getMe?.data?.email}
+            placeholder="zayedhossain120@gmail.com"
+            disabled={true}
+            readonly
+          />
+          <CustomInput
+            label="Phone Number"
+            type="text"
+            name="phoneNumber"
+            defaultValue={getMe?.data?.phoneNumber}
+            placeholder="017*******"
+          />
+        </div>
 
-      <div className="grid grid-cols-1 gap-4 md:gap-6 lg:grid-cols-2">
-        <CustomInput
-          label="Full Name"
-          type="text"
-          name="fullName"
-          placeholder="Zayed Hossain"
-        />
-        <CustomInput
-          label="Email"
-          type="email"
-          name="email"
-          placeholder="zayedhossain120@gmail.com"
-        />
-        <CustomInput
-          label="Phone Number"
-          type="text"
-          name="phoneNumber"
-          placeholder="017*******"
-        />
-      </div>
-
-      <div className="flex justify-end items-center mt-10">
-        <Button className="bg-gradient-primary text-white py-2.5 px-5 my-3 rounded-full w-full lg:max-w-fit ">
-          Update Account Details
-        </Button>
-      </div>
+        <div className="flex justify-end items-center mt-10">
+          <Button className="bg-gradient-primary text-white py-2.5 px-5 my-3 rounded-full w-full lg:max-w-fit ">
+            Update Account Details
+          </Button>
+        </div>
+      </Form>
     </div>
   );
 };
