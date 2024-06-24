d0ecf9f5e3f7d39de47177f96973cae1e1db1648
Shariful525 <sharifulafsar0@gmail.com>
2024-06-24 14:38:02 +0600

    in mega  menu featured products added

---
+++ src/app/(main-layout)/_components/HeaderComponents/FeaturedProduct.tsx
@@ -0,0 +1,25 @@
+import { demoProductPhoto } from "@/assets";
+import Image from "next/image";
+
+export const FeaturedProduct = () => {
+  return (
+    <div className="flex items-center border rounded border-black-10 p-3 gap-3">
+      <div className="w-20 h-20 relative ">
+        <Image
+          src={demoProductPhoto}
+          alt="featured-products"
+          fill
+          className="object-cover"
+        />
+      </div>
+      <div className="flex flex-col gap-2">
+        <p className="text-sm text-black-80">
+          Samsung Electronics Samsung Galexy S21 5G
+        </p>
+        <strong className="text-sm text-gradient-primary font-semibold">
+          $160
+        </strong>
+      </div>
+    </div>
+  );
+};
---
+++ src/app/(main-layout)/_components/HeaderComponents/MegaDiscountCard.tsx
@@ -0,0 +1,40 @@
+import { demoProductPhoto } from "@/assets";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const MegaDiscountCard = () => {
+  return (
+    <div className="bg-gradient-secondary-light p-8 flex flex-col gap-4 items-center max-w-[320px]">
+      <div className="relative w-[195px] h-[150px]">
+        <Image
+          src={demoProductPhoto}
+          alt="mega-discount-product"
+          fill
+          className="object-cover top-0 left-0"
+        />
+      </div>
+      <div className="flex flex-col items-center gap-2">
+        <h3 className="text-2xl font-semibold">21% Discount</h3>
+        <p>
+          Escape the noise, It’s time to hear the magic with Xiaomi Earbuds.
+        </p>
+      </div>
+      <div className="flex flex-col gap-6 items-center">
+        <div className="flex items-center justify-center gap-2">
+          <span>Starting price:</span>
+          <strong className="bg-white px-3 py-1.5 rounded">$99 USD</strong>
+        </div>
+
+        <Link
+          href={"#"}
+          className="flex py-2.5 px-10 rounded items-center justify-center gap-3 text-white bg-gradient-secondary uppercase"
+        >
+          Shop now &rarr;
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default MegaDiscountCard;
