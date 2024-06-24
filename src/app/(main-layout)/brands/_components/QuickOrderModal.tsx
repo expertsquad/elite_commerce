438f47fbfb3f9f52ac943c43f575ee9459024854
Zayed Hossain <zayedhossain120@gmail.com>
2024-06-24 19:22:16 +0600

    working on quick order

--- src/app/(main-layout)/brands/_components/QuickOrderModal.tsx
+++ src/app/(main-layout)/brands/_components/QuickOrderModal.tsx
@@ -1,63 +1,18 @@
 import Modal from "@/Components/Modal";
-import StarRating from "@/Components/StarRating";
 import {
   IconArrowLeft,
   IconBolt,
   IconMapPin,
   IconPhone,
   IconUser,
-  IconX,
 } from "@tabler/icons-react";
-import Image from "next/image";
 import React from "react";
-import IncreaseDecrease from "./IncreaseDecrease";
-import { demoProductPhoto } from "@/assets";
 import ButtonPrimary from "./ButtonPrimary";
 import Link from "next/link";
 import { IProduct } from "@/interfaces/product.interface";
-import { getLocalStorageData } from "@/helpers/localStorage.helper";
-import { storages } from "@/constants";
-import { QuickOrderItem } from "./QuickOrderItems";
 
-export const OrderSummery = () => {
-  return (
-    <div className="md:border border-black-10 rounded-[10px] px-5 py-3.5  space-y-2.5 bg-white">
-      <div className="flex items-center justify-between ">
-        <span className="text-black-80 md:text-base text-sm">Subtotal</span>
-        <strong className="text-black-80 md:text-base text-sm font-semibold">
-          $<span>1300</span>
-        </strong>
-      </div>
-      <div className="flex items-center justify-between ">
-        <span className="text-black-80 md:text-base text-sm">Shipping</span>
-        <strong className="text-black-80 md:text-base text-sm font-semibold">
-          $<span>14</span>
-        </strong>
-      </div>
-      <div className="flex items-center justify-between ">
-        <span className="text-black-80 md:text-base text-sm">Discount</span>
-        <strong className="text-gradient-secondary md:text-base text-sm font-semibold">
-          -$<span>50</span>
-        </strong>
-      </div>
-      <hr className="border-black-10 border my-3.5" />
-      <div className="flex items-center justify-between ">
-        <span className="text-black-80 md:text-lg text-base font-semibold">
-          Total
-        </span>
-        <strong className="text-black-80 md:text-lg text-base font-semibold">
-          $<span>1264</span>
-        </strong>
-      </div>
-      <div className="md:block hidden">
-        <ButtonPrimary buttonType="submit">
-          <IconBolt height={18} width={18} />
-          <span className="uppercase">Confirm Order - $1264.00</span>
-        </ButtonPrimary>
-      </div>
-    </div>
-  );
-};
+import { QuickOrderItem } from "./QuickOrderItems";
+import { OrderSummery } from "./OrderSummery";
 
 const QuickOrderModal = ({
   show,
