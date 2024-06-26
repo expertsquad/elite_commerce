7dd02dd1d09399a0e8bbaddc5bd906b1dd90364d
Shariful525 <sharifulafsar0@gmail.com>
2024-06-26 22:16:12 +0600

    order history sorted by types

---
+++ src/constants/orderHistory.constants.ts
@@ -0,0 +1,22 @@
+export const orderHistoryMenus = [
+  {
+    label: "All Order",
+    value: "all order",
+  },
+  {
+    label: "Order Placed",
+    href: "order-placed",
+  },
+  {
+    label: "Packaging",
+    href: "packaging",
+  },
+  {
+    label: "Shipped",
+    href: "shipped",
+  },
+  {
+    label: "Order Received",
+    href: "order received",
+  },
+];
