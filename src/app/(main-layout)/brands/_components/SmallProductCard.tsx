35df47381d3de32f09703c28f3c71a0d518c27c5
Shariful525 <sharifulafsar0@gmail.com>
2024-06-27 13:38:56 +0600

    top selling brand products added

--- src/app/(main-layout)/brands/_components/SmallProductCard.tsx
+++ src/app/(main-layout)/brands/_components/SmallProductCard.tsx
@@ -6,7 +6,6 @@ import Link from "next/link";
 import React from "react";
 
 const SmallProductCard = ({ product }: { product: IProduct }) => {
-  console.log(product);
   return (
     <Link
       href={`/products/${product?._id}`}
