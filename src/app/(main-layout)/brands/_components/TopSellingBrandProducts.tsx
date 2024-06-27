907fa63ff6fbcc0f5a747c2eaaaeb3698f90a059
Shariful525 <sharifulafsar0@gmail.com>
2024-06-27 13:38:35 +0600

    top selling brand products added

--- src/app/(main-layout)/brands/_components/TopSellingBrandProducts.tsx
+++ src/app/(main-layout)/brands/_components/TopSellingBrandProducts.tsx
@@ -1,15 +1,23 @@
 import React from "react";
 import SmallProductCard from "./SmallProductCard";
+import { fetchData } from "@/actions/fetchData";
+import { IProduct } from "@/interfaces/product.interface";
 
-const TopSellingBrandProducts = () => {
+const TopSellingBrandProducts = async () => {
+  const topSellProducts = await fetchData({
+    route: "/product",
+    query: "sortBy=totalSoldQuantity",
+    limit: 5,
+  });
+
   return (
     <div className="flex flex-col gap-7 ">
       <span className="uppercase text-lg font-semibold">
         Top Selling Brands Product
       </span>
       <div className="flex flex-col gap-7">
-        {[...Array(5)].map((_, index) => {
-          return <SmallProductCard key={index} />;
+        {topSellProducts?.data?.map((product: IProduct, index: number) => {
+          return <SmallProductCard product={product} key={index} />;
         })}
       </div>
     </div>
