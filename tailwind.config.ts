03f0d379487de28626c8295214afa2a75b2ba781
nurulislamrimon <nurulislamrimon@gmail.com>
2024-06-29 14:50:27 +0600

    tailwind config updated

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,51 +8,50 @@ const config: Config = {
   ],
   theme: {
     // use color variables please
-    // colors
-    colors: {
-      primary: {
-        light: "var(--primary-light)",
-        DEFAULT: "var(--primary)",
-      },
-      secondary: {
-        light: "var(--secondary-light)",
-        DEFAULT: "var(--secondary)",
-      },
-      positive: "var(--positive)",
-      danger: "var(--danger)",
-      transparent: "var(--transparent)",
-      white: {
-        transparent: "var(--white-transparent)",
-        15: "var(--white-15)",
-        DEFAULT: "var(--white)",
-      },
-      black: {
-        transparent: "var(--black-transparent)",
-        10: "var(--black-10)",
-        50: "var(--black-50)",
-        80: "var(--black-80)",
-        DEFAULT: "var(--black)",
-      },
-    },
-    // gradient colors
-    backgroundImage: {
-      "gradient-primary": "var(--gradient-primary)",
-      "gradient-primary-light": "var(--gradient-primary-light)",
-      "gradient-secondary": "var(--gradient-secondary)",
-      "gradient-secondary-light": "var(--gradient-secondary-light)",
-      "gradient-positive": "var(--gradient-positive)",
-    },
-    // z index
-    zIndex: {
-      "-1": "-1",
-      0: "0",
-      1: "1",
-      10: "10",
-      20: "20",
-      50: "50",
-    },
-
     extend: {
+      // colors
+      colors: {
+        primary: {
+          light: "var(--primary-light)",
+          DEFAULT: "var(--primary)",
+        },
+        secondary: {
+          light: "var(--secondary-light)",
+          DEFAULT: "var(--secondary)",
+        },
+        positive: "var(--positive)",
+        danger: "var(--danger)",
+        transparent: "var(--transparent)",
+        white: {
+          transparent: "var(--white-transparent)",
+          15: "var(--white-15)",
+          DEFAULT: "var(--white)",
+        },
+        black: {
+          transparent: "var(--black-transparent)",
+          10: "var(--black-10)",
+          50: "var(--black-50)",
+          80: "var(--black-80)",
+          DEFAULT: "var(--black)",
+        },
+      },
+      // gradient colors
+      backgroundImage: {
+        "gradient-primary": "var(--gradient-primary)",
+        "gradient-primary-light": "var(--gradient-primary-light)",
+        "gradient-secondary": "var(--gradient-secondary)",
+        "gradient-secondary-light": "var(--gradient-secondary-light)",
+        "gradient-positive": "var(--gradient-positive)",
+      },
+      // z index
+      zIndex: {
+        "-1": "-1",
+        0: "0",
+        1: "1",
+        10: "10",
+        20: "20",
+        50: "50",
+      },
       aspectRatio: {
         "3/4": "3/4",
         "4/5": "4/5",
