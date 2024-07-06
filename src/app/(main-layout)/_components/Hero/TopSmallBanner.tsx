74be379acad8e6fddd0af834b9cc5587c8415978
JACKIOS7 FD <mrporanyt120@gmail.com>
2024-07-06 13:28:54 +0600

    working on home page hero section carousel and top bottom seciton

--- src/app/(main-layout)/_components/Hero/TopSmallBanner.tsx
+++ src/app/(main-layout)/_components/Hero/TopSmallBanner.tsx
@@ -8,7 +8,7 @@ import Link from "next/link";
 const TopSmallBanner = ({ topOffer }: { topOffer: heroTopSmallBanner }) => {
   return (
     <div
-      className={`flex justify-center md:justify-between items-center p-5 gap-4 rounded-lg flex-1 w-full h-full`}
+      className={`flex items-center gap-x-5 p-5 md:justify-start justify-center rounded-lg flex-1 w-full h-full`}
       style={{
         backgroundColor: `${
           topOffer?.backgroundColor && `${topOffer?.backgroundColor}`
@@ -23,7 +23,7 @@ const TopSmallBanner = ({ topOffer }: { topOffer: heroTopSmallBanner }) => {
         backgroundSize: "cover",
       }}
     >
-      <div className="relative w-36 h-36">
+      <div className="relative w-36 h-full">
         <div className="absolute w-full h-full top-0 left-0 object-cover">
           <Image
             src={`${server_url + topOffer?.productPhoto}`}
@@ -34,17 +34,15 @@ const TopSmallBanner = ({ topOffer }: { topOffer: heroTopSmallBanner }) => {
         </div>
       </div>
 
-      <div className="flex flex-col gap-y-5">
-        <h2 className="  line-clamp-1 [font-size:_clamp(1.2em,2.5vw,0.1.6em)]">
-          {topOffer?.title}
-        </h2>
-        <div className="flex items-baseline gap-1 main-text-color font-bold">
-          <span className="text-lg">$ {topOffer?.price}</span>
+      <div className="flex flex-col">
+        <h2 className="text-2xl line-clamp-2">{topOffer?.title}</h2>
+        <div className="flex items-baseline gap-1 main-text-color font-bold mt-2.5">
+          <span className="text-lg">${topOffer?.price}</span>
         </div>
 
-        <Link href={topOffer?.link}>
-          <Button className="flex items-center justify-center gap-2 bg-gradient-primary text-white rounded-full py-2 px-4 md:px-6 [font-size:_clamp(0.5em,60vw,0.9em)]">
-            {topOffer?.buttonText} <IconArrowRight />{" "}
+        <Link href={topOffer?.link} target="_blank" className="mt-10">
+          <Button className="flex items-center justify-center gap-2 bg-gradient-primary text-white rounded-xl py-2 px-4 md:px-6 [font-size:_clamp(0.5em,60vw,0.9em)]">
+            {topOffer?.buttonText} <IconArrowRight size={20} />
           </Button>
         </Link>
       </div>
