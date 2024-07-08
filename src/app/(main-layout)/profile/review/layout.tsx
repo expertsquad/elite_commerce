57e6ca66db9321adba8fbf1a07a1953ece00ba09
nizamuddin15bd <nizamuddin15bd@gmail.com>
2024-07-08 21:57:10 +0600

    today working end

--- src/app/(main-layout)/profile/review/layout.tsx
+++ src/app/(main-layout)/profile/review/layout.tsx
@@ -1,5 +1,4 @@
 import { Button } from "@/Components/Buttons";
-import Link from "next/link";
 import React from "react";
 
 import ReviewCustomLink from "./_components/reviewCustomLink";
@@ -8,13 +7,13 @@ const ReviewLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
       <div className="flex gap-4 items-center justify-start">
-        <ReviewCustomLink className=" " path="/profile/review/reviewPage">
+        <ReviewCustomLink path="/profile/review/reviewPage">
           <Button className="px-5 py-2 rounded-full border border-black-10">
             To Review
           </Button>
         </ReviewCustomLink>
-        <ReviewCustomLink className=" " path="/profile/review/allReviewHistory">
-          <Button className="px-5 py-2 rounded-full border border-black-10  ">
+        <ReviewCustomLink path="/profile/review/allReviewHistory">
+          <Button className="px-5 py-2 rounded-full border border-black-10">
             Review History
           </Button>
         </ReviewCustomLink>
--- src/app/(main-layout)/profile/review/page.tsx
+++ src/app/(main-layout)/profile/review/page.tsx
@@ -1,8 +1,4 @@
 import React from "react";
-
-import Link from "next/link";
-import { Button } from "@/Components/Buttons";
-// import ProductReviewComponents from "./_components/ProductReviewComponents";
 import AllReviewHistory from "./allReviewHistory/page";
 import ProductReviewComponents from "./reviewPage/page";
 
