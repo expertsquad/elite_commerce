dfb6a251940256f4b612f19a002fd9cddaa4edd2
JACKIOS7 FD <mrporanyt120@gmail.com>
2024-10-17 11:39:20 +0600

    added remote patterns

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+/** @type {import('next').NextConfig} */ const nextConfig = {
   images: {
-    domains: ["api.theqprint.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
   },
 };
 
