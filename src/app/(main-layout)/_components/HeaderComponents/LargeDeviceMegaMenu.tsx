0e49c9295755b3267ac331c0c6e36d00e4de2df1
Shariful525 <sharifulafsar0@gmail.com>
2024-10-23 21:53:18 +0600

    megamenu updated

--- src/app/(main-layout)/_components/HeaderComponents/LargeDeviceMegaMenu.tsx
+++ src/app/(main-layout)/_components/HeaderComponents/LargeDeviceMegaMenu.tsx
@@ -1,8 +1,4 @@
-import {
-  IconChevronDown,
-  IconChevronRight,
-  IconChevronUp,
-} from "@tabler/icons-react";
+import { IconChevronDown, IconChevronRight } from "@tabler/icons-react";
 import Link from "next/link";
 import React from "react";
 import FeaturedCard from "./FeaturedCard";
@@ -15,6 +11,7 @@ import WishlistBtn from "./WishlistBtn";
 import GlobalSearch from "../GlobalSearch/GlobalSearch";
 import ProfilePhotoOrIcon from "./ProfilePhotoOrIcon";
 import { getWidget } from "@/utils/getWidget";
+import MegaMenuItem from "./MegaMenuItem";
 
 const CategoriesAndSubcategories = async () => {
   const categories = await fetchData({
@@ -22,38 +19,17 @@ const CategoriesAndSubcategories = async () => {
     limit: 1000,
     revalidate: 600,
   });
-
   const widget = await getWidget();
 
   return (
-    <div className="absolute bg-white opacity-0 h-0 invisible transition-all duration-300 group-hover/categorybtn:visible group-hover/categorybtn:opacity-100 group-hover/categorybtn:h-[clamp(100px,70vh,500px)] backdrop-blur-xl shadow-2xl py-2 rounded-tl-md rounded-bl-md">
-      <ul className="w-44 h-full overflow-auto">
+    <div className="absolute  bg-white opacity-0 h-0 invisible transition-all duration-300 group-hover/categorybtn:visible group-hover/categorybtn:opacity-100 group-hover/categorybtn:h-[clamp(100px,70vh,500px)] backdrop-blur-xl shadow-2xl py-2 gap-10 rounded-md">
+      <ul className="w-[240px] h-full overflow-auto ">
         {categories?.data?.map((category: ICategory) => (
-          <li key={category?._id} className="group/category">
-            <Link
-              href={`/category/${category?.categoryName}`}
-              className="flex w-full p-2 group-hover/category:bg-gradient-primary-light justify-between"
-            >
-              {category?.categoryName}
-              <IconChevronRight className="text-black-50 hidden group-hover/category:block" />
-            </Link>
-            {/* ================= sub categories ================== */}
-            <div className="fixed top-0 left-[180px] bg-white opacity-0 h-0 invisible transition-all rounded-none duration-300 group-hover/category:visible group-hover/category:opacity-100 group-hover/category:h-[clamp(100px,70vh,500px)] backdrop-blur-xl py-2 shadow-2xl rounded-tr-md rounded-br-md  flex ">
-              <ul className="w-48 h-full flex flex-col overflow-auto">
-                {category?.subcategories?.map((subcategory) => (
-                  <li key={subcategory?.subcategoryId}>
-                    <Link
-                      href={`/category/subcategory/${subcategory?.subcategoryName}`}
-                      className="block w-full p-2 hover:bg-gradient-primary-light"
-                    >
-                      {subcategory?.subcategoryName}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
-              <FeaturedCard widget={widget} />
-            </div>
-          </li>
+          <MegaMenuItem
+            key={category?._id}
+            widget={widget}
+            category={category}
+          />
         ))}
       </ul>
     </div>
@@ -80,7 +56,6 @@ const LargeDeviceMegaMenu = async () => {
             >
               All Categories{" "}
               <IconChevronDown className="text-black-50 group-hover/categorybtn:rotate-180 transition-all duration-700" />
-              {/* <IconChevronUp className="text-black-50 hidden group-hover/categorybtn:block" /> */}
             </Link>
 
             {/* ========================================================== */}
