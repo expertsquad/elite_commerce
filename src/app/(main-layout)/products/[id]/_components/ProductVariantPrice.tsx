d810b4ac805fc7237944f6ca21286c12ab154b40
JACKIOS7 FD <mrporanyt120@gmail.com>
2024-10-24 13:03:34 +0600

    working on bug fixing

--- src/app/(main-layout)/products/[id]/_components/ProductVariantPrice.tsx
+++ src/app/(main-layout)/products/[id]/_components/ProductVariantPrice.tsx
@@ -18,31 +18,33 @@ const ProductVariantPrice = ({
         {currencyIcon}
         {variant?.discountedPrice
           ? variant?.discountedPrice
-          : product?.variants[0].discountedPrice}
+          : product?.variants[0]?.sellingPrice}
       </span>
       <span className="text-black-10">|</span>
-      <del className="[font-size:_clamp(20px,3vw,28px)] text-black-50">
+      <del className="[font-size:_clamp(20px,3vw,25px)] text-black-50">
         {currencyIcon}
         {variant?.sellingPrice
           ? variant?.sellingPrice
           : product?.variants[0]?.sellingPrice}
       </del>
-      <span className="text-black-10">|</span>
-      <div
-        title={`You will get extra ${
-          variant?.discountPercentage
-            ? variant?.discountPercentage
-            : product?.variants[0]?.discountPercentage
-        } more!!`}
-        className="bg-gradient-secondary-light rounded-full py-1"
-      >
-        <span className="text-base md:text-lg text-gradient-secondary px-5 font-semibold">
-          {variant?.discountPercentage
-            ? variant?.discountPercentage
-            : product?.variants[0]?.discountPercentage}
-          % OFF
-        </span>
-      </div>
+      {variant?.discountPercentage && <span className="text-black-10">|</span>}
+      {variant?.discountPercentage && (
+        <div
+          title={`You will get extra ${
+            variant?.discountPercentage
+              ? variant?.discountPercentage
+              : product?.variants[0]?.discountPercentage
+          } more!!`}
+          className="bg-gradient-secondary-light rounded-full py-1"
+        >
+          <span className="text-base md:text-lg text-gradient-secondary px-5 font-semibold">
+            {variant?.discountPercentage
+              ? variant?.discountPercentage
+              : product?.variants[0]?.discountPercentage}
+            % OFF
+          </span>
+        </div>
+      )}
     </div>
   );
 };
