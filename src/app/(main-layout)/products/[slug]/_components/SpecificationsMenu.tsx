152b7f8717b348c17bc82044aba783d47493f704
JACKIOS7 FD <mrporanyt120@gmail.com>
2024-10-27 18:11:59 +0600

    working on product view specification section

--- src/app/(main-layout)/products/[slug]/_components/SpecificationsMenu.tsx
+++ src/app/(main-layout)/products/[slug]/_components/SpecificationsMenu.tsx
@@ -1,15 +1,18 @@
+"use client";
+import TabsWithUnderline from "@/Components/TabsWithUnderline";
 import { specificationsMenu } from "@/constants/specifications.menu.contstant";
 import Link from "next/link";
+import React from "react";
 
 const SpecificationsMenu = () => {
+  const [activeTab, setActiveTab] = React.useState(0);
   return (
     <div className="overflow-x-auto scrollbar-x-remove border-b-2 border-black-10 my-6 pb-2 flex items-center gap-x-10">
-      {specificationsMenu.map((item) => (
+      {specificationsMenu.map((item, index) => (
         <Link
-          className={`text-base md:text-lg text-black-80 shrink-0 text-nowrap hover:text-primary-light ${
-            item?._id === item?.index
-              ? "text-gradient-primary"
-              : "text-gradient-primary"
+          onClick={() => setActiveTab(item.index)}
+          className={`text-base md:text-lg shrink-0 text-nowrap text-gradient-primary ${
+            activeTab === item.index ? "text-gradient-primary" : "text-black"
           }`}
           key={item._id}
           href={item.hashtag}
