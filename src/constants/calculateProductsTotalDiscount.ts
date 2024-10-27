a3de4c6481f8bd60a1488ece0258f11f83f5e41a
JACKIOS7 FD <mrporanyt120@gmail.com>
2024-10-27 12:50:44 +0600

    api updated

--- src/constants/calculateProductsTotalDiscount.ts
+++ src/constants/calculateProductsTotalDiscount.ts
@@ -3,18 +3,20 @@ import { IProduct } from "@/interfaces/product.interface";
 export const calculateProductsTotalDiscount = (
   products: IProduct[]
 ): number => {
-  return products.reduce((totalDiscount, product) => {
-    const variantDiscounts = product.variants.map((variant) => {
-      const { sellingPrice, discountedPrice } = variant;
-      return sellingPrice && discountedPrice
-        ? sellingPrice - discountedPrice
-        : 0;
-    });
+  return products?.reduce((totalDiscount, product) => {
+    const variantDiscounts =
+      product?.variants?.map((variant) => {
+        const { sellingPrice = 0, discountedPrice = 0 } = variant;
+        return sellingPrice > discountedPrice
+          ? sellingPrice - discountedPrice
+          : 0;
+      }) || [];
 
-    const productDiscountTotal = variantDiscounts.reduce(
-      (acc, discount) => acc + discount,
+    const productDiscountTotal = variantDiscounts?.reduce(
+      (acc, discount) => acc + (discount || 0),
       0
     );
-    return totalDiscount + productDiscountTotal;
+
+    return totalDiscount + (productDiscountTotal || 0);
   }, 0);
 };
