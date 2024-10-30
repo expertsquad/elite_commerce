a2f43a59923623d4bfaecbf56de0e91e2c47f21d
JACKIOS7 FD <mrporanyt120@gmail.com>
2024-10-30 18:48:07 +0600

    merged and builded successfully

--- src/app/___ExampleComponent/a/layout.tsx
+++
@@ -1,7 +0,0 @@
-import React, { Fragment } from "react";
-
-const Layout = ({ children }: { children: React.ReactNode }) => {
-  return <Fragment>{children}</Fragment>;
-};
-
-export default Layout;
