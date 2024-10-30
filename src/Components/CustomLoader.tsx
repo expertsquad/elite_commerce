4d51134749aaf203d344ef25294caf045e82f42a
Shariful525 <sharifulafsar0@gmail.com>
2024-10-30 18:23:46 +0600

    loader updated

--- src/Components/CustomLoader.tsx
+++ src/Components/CustomLoader.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const CustomLoader = () => {
   return (
     <div className=" bg-white absolute opacity-60 z-50 h-full w-full flex items-center justify-center">
-      <div className="border-black-70 h-10 w-10 animate-spin rounded-full border-4 border-t-primary-light" />
+      <div className="border-black-70 h-10 w-10 animate-spin rounded-full border-4 border-t-primary-[#8b7cfd]" />
     </div>
   );
 };
