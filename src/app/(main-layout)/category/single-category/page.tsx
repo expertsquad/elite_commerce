499d3385e3bb774ac929cb01b7ee362afd16d503
Shariful525 <sharifulafsar0@gmail.com>
2024-11-02 12:56:17 +0600

    single category page updated

--- src/app/(main-layout)/category/single-category/page.tsx
+++ src/app/(main-layout)/category/single-category/page.tsx
@@ -4,6 +4,7 @@ import ProductCard from "@/Components/ProductCard/ProductCard";
 import { IProduct } from "@/interfaces/product.interface";
 import React from "react";
 import ProductEmptyState from "../../_components/ProductEmptyState";
+import { getCurrency } from "@/utils/getCurrency";
 
 export async function generateMetadata({
   searchParams,
@@ -51,13 +52,25 @@ const SingleCategory = async ({
   });
 
   const totalPages = Math.ceil(response?.meta?.total / response?.meta?.limit);
+  const currency = await getCurrency();
+  const quickOrderServices = await fetchData({
+    route: "/settings/quick-order-setting",
+  });
 
   return (
     <div>
       {response?.data?.length > 0 ? (
         <div className="grid grid-cols-product-grid gap-5 place-items-center">
           {response?.data?.map((product: IProduct) => (
-            <ProductCard key={product?._id} product={product} />
+            <ProductCard
+              key={product?._id}
+              product={product}
+              currencyIcon={currency}
+              isQuickOrderActive={
+                quickOrderServices?.data?.isQuickOrderServiceActive
+              }
+              shippingAmount={quickOrderServices?.data?.deliveryCharge}
+            />
           ))}
         </div>
       ) : (
--- src/app/(main-layout)/category/single-category/page/[page]/page.tsx
+++ src/app/(main-layout)/category/single-category/page/[page]/page.tsx
@@ -3,6 +3,7 @@ import ProductEmptyState from "@/app/(main-layout)/_components/ProductEmptyState
 import Pagination from "@/Components/Pagination";
 import ProductCard from "@/Components/ProductCard/ProductCard";
 import { IProduct } from "@/interfaces/product.interface";
+import { getCurrency } from "@/utils/getCurrency";
 import React from "react";
 
 const SingleDynamicCategory = async ({
@@ -19,13 +20,25 @@ const SingleDynamicCategory = async ({
     page: Number(params?.page),
   });
   const totalPages = Math.ceil(response?.meta?.total / response?.meta?.limit);
+  const currency = await getCurrency();
+  const quickOrderServices = await fetchData({
+    route: "/settings/quick-order-setting",
+  });
 
   return (
     <div>
       {response?.data?.length > 0 ? (
         <div className="grid grid-cols-product-grid gap-5 place-items-center">
           {response?.data?.map((product: IProduct) => (
-            <ProductCard key={product?._id} product={product} />
+            <ProductCard
+              key={product?._id}
+              product={product}
+              currencyIcon={currency}
+              isQuickOrderActive={
+                quickOrderServices?.data?.isQuickOrderServiceActive
+              }
+              shippingAmount={quickOrderServices?.data?.deliveryCharge}
+            />
           ))}
         </div>
       ) : (
