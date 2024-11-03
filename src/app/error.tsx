995e734fadea5a4cebeeb75e343902f5ff8a2d96
Shariful525 <sharifulafsar0@gmail.com>
2024-11-03 21:25:29 +0600

    global error page updated

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -3,6 +3,8 @@
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import Image from "next/image";
+import errorImg from "@/assets/Images/errorImg.svg";
 
 export default function Error({
   error,
@@ -23,7 +25,11 @@ export default function Error({
 
     // Redirect to home page when countdown reaches 0
     if (countdown === 0) {
-      router.push("/");
+      if (error.message === "Access token not found, please login again!") {
+        router.push("/login");
+      } else {
+        router.push("/");
+      }
     }
 
     // Clear interval when component unmounts or countdown completes
@@ -32,15 +38,32 @@ export default function Error({
 
   return (
     <div className="min-h-full w-full flex items-center justify-center flex-col gap-5">
-      <h2 className="text-gradient-secondary">{error.message}!</h2>
+      <div className="relative w-[450px] h-[450px]">
+        <Image
+          src={errorImg}
+          alt="Error Image"
+          fill
+          className="inset-0 object-contain w-full h-full"
+        />
+      </div>
+      <div className="flex flex-col items-center gap-4">
+        <h2 className="text-danger text-3xl font-bold">ERROR!</h2>
+        <div className="text-danger">
+          {error.message === "Access token not found, please login again!"
+            ? "Please Login"
+            : error.message}
+          !
+        </div>
+      </div>
       <button onClick={() => reset()} className="text-gradient-primary">
         Try again
       </button>
-      <Link href="/login" className="bg-positive text-white px-5 rounded-md">
-        Login Again
-      </Link>
       <p className="text-sm text-black-50">
-        Redirecting to the home page in {countdown}
+        Redirecting to the{" "}
+        {error.message === "Access token not found, please login again!"
+          ? "login"
+          : "home"}{" "}
+        page in {countdown}
       </p>
     </div>
   );
