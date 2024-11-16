ce31f30a15a5d753c6bd9d05541a4e4ef356a1fd
Shariful525 <sharifulafsar0@gmail.com>
2024-11-16 18:56:01 +0600

    about-us, privacy-policy, terms-and-conditions updated

--- src/app/(main-layout)/_components/HeaderComponents/TopThinNav.tsx
+++ src/app/(main-layout)/_components/HeaderComponents/TopThinNav.tsx
@@ -1,22 +1,54 @@
-import { storeSocialMedia, topMenus } from "@/constants/mainMenus.constants";
+import { fetchData } from "@/actions/fetchData";
+import { storeSocialMedia } from "@/constants/mainMenus.constants";
 import Link from "next/link";
 import React from "react";
 
-const TopThinNav = () => {
+const TopThinNav = async () => {
+  const aboutUs = await fetchData({ route: "/settings/about-us" });
+  const termsAndConditions = await fetchData({
+    route: "/settings/terms-and-conditions",
+  });
+  const privacyPolicy = await fetchData({
+    route: "/settings/privacy-policy",
+  });
+
+  const isAboutUsActive = aboutUs?.data?.isActive;
+  const isTermsAndConditionsActive = termsAndConditions?.data?.isActive;
+  const isPrivacyPolicyActive = privacyPolicy?.data?.isActive;
+
   return (
     <div id="header">
       <div className="hidden md:flex justify-between items-center bg-black-10  text-black text-xs px-3 py-1">
         <div className="flex items-center gap-6">
-          {topMenus.map((menu, i: number) => (
-            <Link href={`${menu?.href}`} className="hover:underline" key={i}>
-              {menu.label}
+          {isAboutUsActive ? (
+            <Link href={`/about-us`} className="hover:underline">
+              About Us
             </Link>
-          ))}
+          ) : (
+            ""
+          )}
+          {isPrivacyPolicyActive ? (
+            <Link href={`/privacy-policy`} className="hover:underline">
+              Privacy Policy
+            </Link>
+          ) : (
+            ""
+          )}
+          {isTermsAndConditionsActive ? (
+            <Link href={`/terms-and-conditions`} className="hover:underline">
+              Terms and Conditions
+            </Link>
+          ) : (
+            ""
+          )}
+          <Link href={`/wishlist`} className="hover:underline">
+            Wishlist
+          </Link>
         </div>
         <ul className="flex items-center gap-6">
           Share
           {storeSocialMedia.map((socialMedia, i: number) => (
-            <li key={i}>
+            <li className="list-none" key={i}>
               <Link
                 href={socialMedia.href}
                 target="_blank"
