58d7973154530ddf7cb8da4cd0d9f76a4163702e
JACKIOS7 FD <mrporanyt120@gmail.com>
2024-11-17 12:38:36 +0600

    slider updated

--- src/app/(main-layout)/_components/Hero/Carousel.tsx
+++ src/app/(main-layout)/_components/Hero/Carousel.tsx
@@ -50,47 +50,53 @@ const Carousel = ({
           <h2
             className={`lg:text-4xl md:text-lg text-lg font-bold leading-0 line-clamp-2`}
           >
-            {item.title}
+            {item?.title}
           </h2>
           <p
             className={`[font-size:_clamp(0.85em,5vw,1em)] text-gray-500 line-clamp-2 `}
           >
-            {item.description}
+            {item?.description}
           </p>
 
-          <div className="hidden md:flex md:items-center items-baseline gap-x-2 md:mt-5 [font-size:_clamp(0.5em,60vw,0.9em)]">
-            <span className="text-2xl font-bold">
-              {currencyIcon}
-              {discountedPrice.toFixed(2)}
-            </span>
+          {discountedPrice && item?.price ? (
+            <div className="hidden md:flex md:items-center items-baseline gap-x-2 md:mt-5 [font-size:_clamp(0.5em,60vw,0.9em)]">
+              <span className="text-2xl font-bold">
+                {currencyIcon}
+                {discountedPrice?.toFixed(2)}
+              </span>
 
-            <del className="text-lg text-black-50 hidden md:block">
-              {currencyIcon}
-              {item?.price.toFixed(2)}
-            </del>
-            <span className="text-xs md:text-base  text-danger">
-              {item?.discountPercentage}% OFF
-            </span>
-          </div>
-          <div className="md:mt-6 hover:text-positive">
-            <Link
-              href={item?.link}
-              className="min-w-[100px] max-w-[180px] flex items-center justify-center gap-x-2 bg-gradient-primary hover:bg-gradient-primary-reverse duration-150 transition-all text-white rounded-lg py-3 [font-size:_clamp(12px,2.5vw,16px)] uppercase whitespace-nowrap"
-            >
-              {item?.buttonText} <IconArrowRight size={20} />
-            </Link>
-          </div>
+              <del className="text-lg text-black-50 hidden md:block">
+                {currencyIcon}
+                {item?.price?.toFixed(2)}
+              </del>
+              <span className="text-xs md:text-base text-danger">
+                {item?.discountPercentage}% OFF
+              </span>
+            </div>
+          ) : null}
+          {item?.buttonText ? (
+            <div className="md:mt-6 hover:text-positive">
+              <Link
+                href={item?.link}
+                className="min-w-[100px] max-w-[180px] flex items-center justify-center gap-x-2 bg-gradient-primary hover:bg-gradient-primary-reverse duration-150 transition-all text-white rounded-lg py-3 [font-size:_clamp(12px,2.5vw,16px)] uppercase whitespace-nowrap"
+              >
+                {item?.buttonText} <IconArrowRight size={20} />
+              </Link>
+            </div>
+          ) : null}
         </div>
-        <div className="relative md:w-[250px] md:h-[250px] w-[180px] h-[150px]">
-          <Image
-            src={`${server_url + item?.productPhoto}`}
-            alt="hero item images"
-            fill
-            priority={true}
-            sizes="(max-width: 768px) 30vw, (max-width: 1200px) 50vw, 33vw"
-            className="w-full h-full top-0 left-0 object-contain"
-          />
-        </div>
+        {item?.productPhoto ? (
+          <div className="relative md:w-[250px] md:h-[250px] w-[180px] h-[150px]">
+            <Image
+              src={`${server_url + item?.productPhoto}`}
+              alt="hero item images"
+              fill
+              priority={true}
+              sizes="(max-width: 768px) 30vw, (max-width: 1200px) 50vw, 33vw"
+              className="w-full h-full top-0 left-0 object-contain"
+            />
+          </div>
+        ) : null}
       </div>
     </div>
   );
