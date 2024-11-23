65472cb374c9f4eda671ddfe4f3378b12279883e
JACKIOS7 DEVELOPER <mrporanyt120@gmail.com>
2024-11-23 20:52:21 +0600

    product view page brand info updated

--- src/app/(main-layout)/products/[slug]/_components/CategoryAndBrandSmallComponent.tsx
+++ src/app/(main-layout)/products/[slug]/_components/CategoryAndBrandSmallComponent.tsx
@@ -24,8 +24,8 @@ const CategoryAndBrandSmallComponent = ({
           src={`${server_url + brandPhoto}`}
           alt="Prand Photo"
           fill
-          style={{ objectFit: "cover" }}
-          className="inset-0 object-cover"
+          style={{ objectFit: "contain" }}
+          className="inset-0 object-contain"
         />
       </Link>
       <span className="text-black-10">|</span>
