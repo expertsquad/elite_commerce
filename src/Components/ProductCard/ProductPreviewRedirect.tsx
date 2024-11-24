c23a7a7ea870df1efe69798e1e24d2ca669eb120
JACKIOS7 DEVELOPER <mrporanyt120@gmail.com>
2024-11-24 17:48:15 +0600

    product card updated

--- src/Components/ProductCard/ProductPreviewRedirect.tsx
+++ src/Components/ProductCard/ProductPreviewRedirect.tsx
@@ -1,7 +1,9 @@
 "use client";
 import { IProduct } from "@/interfaces/product.interface";
 import Link from "next/link";
-import React from "react";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
+import CustomLoading from "../CustomLoader";
 
 const ProductPreviewRedirect = ({
   className,
@@ -12,18 +14,25 @@ const ProductPreviewRedirect = ({
   children: React.ReactNode;
   product: IProduct;
 }) => {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
   const handleRedirect = (e: React.MouseEvent) => {
-    e.stopPropagation();
+    e.stopPropagation(); // Prevent event propagation
+    setIsLoading(true); // Show custom loading
+
+    // Simulate loading for 2 seconds before navigating
+    setTimeout(() => {
+      setIsLoading(false); // Hide loader
+      router.push(`/products/${product?.productUrlSlug}`); // Navigate after delay
+    }, 3000);
   };
 
   return (
-    <Link
-      href={`/products/${product?.productUrlSlug}`}
-      className={className}
-      onClick={handleRedirect}
-    >
+    <div className={`${className} relative`} onClick={handleRedirect}>
+      {isLoading && <CustomLoading />}
       {children}
-    </Link>
+    </div>
   );
 };
 
